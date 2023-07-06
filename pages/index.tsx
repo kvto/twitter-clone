e246528295e27f53ace8f395977d3c45d9ae6cf7
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2023-07-06 16:02:51 -0500

    Load and create post

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
+import Form from "@/components/Form";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <>
      <Header showBackArrow label="Home"/>
+     <Form placeholder="What's happening?"/>
+     <PostFeed />
     </>
   )
 }
