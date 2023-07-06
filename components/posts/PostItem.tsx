47f81f19e819657f78192d70bc281bd327f79587
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2023-07-06 17:44:57 -0500

    Load and Posts

---
+++ components/posts/PostItem.tsx
@@ -0,0 +1,56 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useLoginModal from "@/hooks/useLoginModal";
+import { formatDistanceToNowStrict } from "date-fns";
+import { useRouter } from "next/router";
+import { useCallback, useMemo } from "react";
+
+interface PostItemProps {
+    data: Record<string, any>;
+    userId?: string;
+}
+
+const PostItem: React.FC<PostItemProps> = ({data, userId}) => {
+    const router = useRouter();
+    const loginModal = useLoginModal();
+
+    const {data: currentUser} = useCurrentUser();
+
+    const goToUser = useCallback((event: any) => {
+        event.stopPropagation();
+
+        router.push(`/users/${data.user.id}`);
+    }, [router, data.user.id]);
+
+    const goToPost = useCallback(() => {
+        router.push(`/posts/${data.id}`);
+    },[router, data.id]);
+
+    const onLike = useCallback((event: any) => {
+        event.stopPropagation();
+
+        loginModal.onOpen();
+    },[loginModal])
+
+    const createdAt = useMemo(() => {
+        if(!data?.createdAt){
+            return null;
+        }
+        return formatDistanceToNowStrict(new Date(data.createdAt));
+    },[data?.createdAt]);
+
+
+    return ( 
+    <div 
+    onClick={goToPost}
+    className="
+    border-b-[1px]
+    border-neutral-800
+    p-5
+    cursor-pointer
+    hover:bg-neutral-900">
+
+    </div> 
+    );
+}
+ 
+export default PostItem;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Form from "@/components/Form";
 import Header from "@/components/Header";
+import PostFeed from "@/components/posts/PostFeed";
 
 export default function Home() {
   return (
